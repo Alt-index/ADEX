ca26d8d67754504b436b5241fa200bde5bd6134f
nOsDelAbismo <srdelabismo@lescovex.com>
2018-03-06 02:14:06 +0100

    contract global changes

--- adex_testing.sol
+++ adex_testing.sol
@@ -1,296 +1,165 @@
 pragma solidity ^0.4.19;
 
-//vicent nos & enrique santos
+// Vicent Nos & Enrique Santos
+
+
 library SafeMath {
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return c;
-  }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
-
 
 
 contract Ownable {
-  address public owner;
+    address public owner;
 
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    function Ownable() internal {
+        owner = msg.sender;
+    }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-
-  function Ownable() internal {
-    owner = msg.sender;
-  }
-
-
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-
-
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 }
 
 
 //////////////////////////////////////////////////////////////
 //                                                          //
-//  ADEX, Shareholder's ERC20  //
+//  Lescovex, Shareholder's ERC20                           //
 //                                                          //
 //////////////////////////////////////////////////////////////
 
-
-contract ADEX is Ownable {
-  uint256 public totalSupply;
-  using SafeMath for uint256;
-
-  mapping(address => uint256) balances;
-
-  mapping(address => uint256) holded;
-
-  mapping(address => uint256) requestWithdraws;
-
-  event Transfer(address indexed from, address indexed to, uint256 value);
-
- event Approval(address indexed owner, address indexed spender, uint256 value);
+contract LescovexERC20 is Ownable {
+    
+    using SafeMath for uint256;
 
 
-  function transfer(address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[msg.sender]);
-    if(block.number>blockEndICO || msg.sender==owner){
-    // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    holded[_to]=block.number;
-    balances[_to] = balances[_to].add(_value);
-    Transfer(msg.sender, _to, _value);
-    return true;
+    mapping (address => uint256) public balances;
+
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    mapping (address => timeHold) holded;
+
+    struct timeHold{
+        uint256[] amount;
+        uint256[] time;
+        uint256 length;
     }
-  }
 
+    uint256 public constant blockEndICO = 1524182460;
 
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
-
-
-  mapping (address => mapping (address => uint256)) internal allowed;
-
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-
-    balances[_from] = balances[_from].sub(_value);
-    holded[_to]=block.number;
-    balances[_to] = balances[_to].add(_value);
-
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-    Transfer(_from, _to, _value);
-    return true;
-  }
-
-
-
-  function approve(address _spender, uint256 _value) public returns (bool) {
-    allowed[msg.sender][_spender] = _value;
-    Approval(msg.sender, _spender, _value);
-    return true;
-  }
-
-
-  function allowance(address _owner, address _spender) public view returns (uint256) {
-    return allowed[_owner][_spender];
-  }
-
-
-  function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
-    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-
-  function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
-    uint oldValue = allowed[msg.sender][_spender];
-    if (_subtractedValue > oldValue) {
-      allowed[msg.sender][_spender] = 0;
-    } else {
-      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-    }
-    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-    return true;
-  }
-
-    string public constant standard = "ERC20 ADEX";
-
-    /* Public variables for the ERC20 token, defined when calling the constructor */
+    /* Public variables for the ERC20 token */
+    string public constant standard = "ERC20 Lescovex";
+    uint8 public constant decimals = 8; // hardcoded to be a constant
+    uint256 public totalSupply;
     string public name;
     string public symbol;
-    uint8 public constant decimals = 8; // hardcoded to be a constant
 
-    // Contract variables and constants
-    uint256 public constant minPrice = 10e12;
-    uint256 public constant blockEndICO = 4000000;
-    uint256 public buyPrice = minPrice;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    uint256 public tokenReward = 0;
-    // constant to simplify conversion of token amounts into integer form
-    uint256 private constant tokenUnit = uint256(10)**decimals;
-    
-    // Spread in parts per 100 millions, such that expressing percentages is 
-    // just to append the postfix 'e6'. For example, 4.53% is: spread = 4.53e6
-    address public ADEXAddr = 0xa9a3027bf228f1E85385BF06b4Db0eF62c66c14C;
 
-    //Declare logging events
-    event LogDeposit(address sender, uint amount);
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function ADEX(uint256 initialSupply, string tokenName, string tokenSymbol) public {
-        balances[msg.sender] = initialSupply; // Give the creator all initial tokens
-        totalSupply = initialSupply;  // Update total supply
-        name = tokenName;             // Set the name for display purposes
-        symbol = tokenSymbol;         // Set the symbol for display purposes
-
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
     }
 
-    function () public payable {
-        buy();   // Allow to buy tokens sending ether direcly to contract
-    }
-    
-
-    modifier status() {
-        _;  // modified function code should go before prices update
-
-    if(balances[this]>900000000000000){
-      buyPrice=1500000000000000;
-    }else if(balances[this]>800000000000000 && balances[this]<=900000000000000){
-
-      buyPrice=2000000000000000;
-    }else if(balances[this]>700000000000000 && balances[this]<=800000000000000){
-
-      buyPrice=2500000000000000;
-    }else if(balances[this]>600000000000000 && balances[this]<=700000000000000){
-
-      buyPrice=3000000000000000;
-    }else{
-
-      buyPrice=4000000000000000;
+    function hold(address _to, uint256 _value) internal {
+        holded[_to].amount.push(_value);
+        holded[_to].time.push(block.number);
+        holded[_to].length++;
     }
 
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(block.timestamp > blockEndICO || msg.sender == owner);
+        require(_to != address(0));
+
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+
+        delete holded[msg.sender];
+        hold(_to,_value);
         
-    }
+        balances[_to] = balances[_to].add(_value);
 
-
-    function setReward(uint256 _value) public{
-
-      tokenReward=_value;
-    }
-
-    function deposit() public payable status returns(bool success) {
-        // Check for overflows;
-        assert (this.balance + msg.value >= this.balance); // Check for overflows
-      
-        //executes event to reflect the changes
-        LogDeposit(msg.sender, msg.value);
-        
+        Transfer(msg.sender, _to, _value);
         return true;
     }
 
-  function withdrawReward() public status{
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        balances[_from] = balances[_from].sub(_value);
+        
+        delete holded[msg.sender];
+        hold(_to,_value);
 
-    
-    ///if(block.number-holded[msg.sender]>172800){ //1 month
-      if(block.number-holded[msg.sender]>10 && block.number-holded[msg.sender]<20){
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
 
-      msg.sender.transfer(tokenReward*requestWithdraws[msg.sender]);
-      
-      //executes event ro register the changes
-      LogWithdrawal(msg.sender, tokenReward*balances[msg.sender]);
-
-      }
-  }
-
-
-  event LogWithdrawal(address receiver, uint amount);
-  
-  function withdraw(uint value) public onlyOwner{
-    //send eth to owner address
-    msg.sender.transfer(value);
-    //executes event ro register the changes
-    LogWithdrawal(msg.sender, value);
-  }
-
-  function requestWithdraw(uint value) public {
-    require(value <= balances[msg.sender]);
-
-    holded[msg.sender]=block.number;
-
-    requestWithdraws[msg.sender]=value;
-    //executes event ro register the changes
-    LogWithdrawal(msg.sender, value);
-
-  }
-
-    function transferBuy(address _to, uint256 _value) internal returns (bool) {
-      require(_to != address(0));
-      require(_value <= balances[this]);
-
-      // SafeMath.sub will throw if there is not enough balance.
-      balances[this] = balances[this].sub(_value);
-      holded[_to]=block.number;
-      balances[_to] = balances[_to].add(_value);
-      Transfer(this, _to, _value);
-      return true;
-      
+        Transfer(_from, _to, _value);
+        return true;
     }
 
-
-    function buy() public payable status{
-      require (msg.sender.balance >= msg.value);  // Check if the sender has enought eth to buy
-      assert (msg.sender.balance + msg.value >= msg.sender.balance); //check for overflows
-         
-      uint256 tokenAmount = (msg.value / buyPrice)*tokenUnit ;  // calculates the amount
-
-      transferBuy(msg.sender, tokenAmount);
-      ADEXAddr.transfer(msg.value);
-    
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
     }
 
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
+
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+        allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+        uint oldValue = allowed[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
 
     /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public onlyOwner returns (bool success) {    
-
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {    
         tokenRecipient spender = tokenRecipient(_spender);
 
         if (approve(_spender, _value)) {
@@ -301,6 +170,157 @@ contract ADEX is Ownable {
 }
 
 
-contract tokenRecipient {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public ; 
+interface tokenRecipient {
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external ; 
 }
+
+    
+contract Lescovex is LescovexERC20 {
+
+    // Contract variables and constants
+    uint256 constant initialSupply = 0;
+    uint256 constant maxSupply = 1000000000000000;
+    string constant tokenName = "Lescovex Shareholder's";
+    string constant tokenSymbol = "LCX";
+    uint256 constant holdTime = 5; // number of blocks required to hold for reward
+    uint256 constant holdMax = 5; // number of blocks required to hold for reward
+
+    address public LescovexAddr = 0xD26286eb9E6E623dba88Ed504b628F648ADF7a0E;
+    uint256 public tokenReward = 0;
+    // constant to simplify conversion of token amounts into integer form
+    uint256 public tokenUnit = uint256(10)**decimals;
+
+
+    //Declare logging events
+    event LogDeposit(address sender, uint amount);
+    event LogWithdrawal(address receiver, uint amount);
+  
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function Lescovex() public {
+        totalSupply = initialSupply;  // Update total supply
+        name = tokenName;             // Set the name for display purposes
+        symbol = tokenSymbol;         // Set the symbol for display purposes
+    }
+
+    function () public payable {
+        buy();   // Allow to buy tokens sending ether directly to contract
+    }
+
+    function buyPrice() public view returns (uint256 price) {
+        
+
+            // price with the discounts applied on each period
+        if(balances[this]>900000000000000){
+         return 1500000000000000;
+        }else if(balances[this]>800000000000000 && balances[this]<=900000000000000){
+
+          return 2000000000000000;
+        }else if(balances[this]>700000000000000 && balances[this]<=800000000000000){
+
+          return 2500000000000000;
+        }else if(balances[this]>600000000000000 && balances[this]<=700000000000000){
+
+          return 3000000000000000;
+        }else{
+
+          return 4000000000000000;
+        }
+
+    }
+
+    function deposit() external payable onlyOwner returns(bool success) {
+        // Check for overflows;
+
+        assert (this.balance + msg.value >= this.balance); // Check for overflows
+        tokenReward = this.balance / totalSupply;
+
+        //executes event to reflect the changes
+        LogDeposit(msg.sender, msg.value);
+        
+        return true;
+    }
+
+    function withdrawReward() external {
+
+        uint i = 0;
+        uint256 ethAmount = 0;
+        uint256 len = holded[msg.sender].length;
+
+        while (i <= len - 1){
+            if (block.number -  holded[msg.sender].time[i] > holdTime && block.number -  holded[msg.sender].time[i] < holdMax){
+                ethAmount += tokenReward * holded[msg.sender].amount[i];
+            }
+            i++;
+        }
+
+        delete holded[msg.sender];
+
+        require(ethAmount > 0);
+        //send eth to owner address
+         msg.sender.transfer(ethAmount*requestWithdraws[msg.sender]);
+          
+        //executes event to register the changes
+        LogWithdrawal(msg.sender, ethAmount);
+    }
+
+
+    function setReward(uint256 _value) public onlyOwner{
+
+      tokenReward=_value;
+    }
+
+
+
+  event LogWithdrawal(address receiver, uint amount);
+
+
+  function requestWithdraw(uint value) public {
+    require(value <= balances[msg.sender]);
+
+    hold(_to,_value);
+
+    requestWithdraws[msg.sender]=value;
+    //executes event ro register the changes
+    
+
+  }
+
+
+    function buy() public payable {
+        require(totalSupply <= maxSupply);
+        require(block.timestamp < blockEndICO);
+
+        uint256 tokenAmount = (msg.value * tokenUnit) / buyPrice();  // calculates the amount
+        transferBuy(msg.sender, tokenAmount);
+
+        LescovexAddr.transfer(msg.value);
+    }
+
+    function transferBuy(address _to, uint256 _value) internal returns (bool) {
+        require(_to != address(0));
+
+        // SafeMath.add will throw if there is not enough balance.
+        totalSupply = totalSupply.add(_value*2);
+
+        hold(_to,_value);
+        balances[LescovexAddr] = balances[LescovexAddr].add(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        Transfer(this, _to, _value);
+        Transfer(this, LescovexAddr, _value);
+        return true;
+    }
+
+    function burn(address addr) external onlyOwner{
+        totalSupply = totalSupply.sub(balances[addr]);
+        balances[addr] = 0;
+    }
+
+}
+
+
+
+
+
+   
